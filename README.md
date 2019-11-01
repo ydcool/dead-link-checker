2339b689073e118780bf784f2f57a45800aaf1e6
Dominic Yin <yindongchao@inspur.com>
2019-11-01 16:37:10 +0800

    add verbose support; update readme;

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Github Dead Link Checker
+
+## 使用说明
+
+```shell script
+$ ./dead-link-checker.exe
+
+Dead Link Checker
+  Code with ❤  by Dominic
+Usage:
+  -ak string
+        access token for higher requests rate
+  -au string
+        access username for higher requests rate
+  -b string
+        the branch of repository to check, default master (default "master")
+  -h    help
+  -l string
+        the user name of target repo path to check , for example 'github.com/docker/docker-ce'
+  -o string
+        write result to file   
+  -v    print all checked links, default false to show broken only
+  -p string
+        http/https proxy
+```
+
+示例：
+
+```shell script
+$ ./dead-link-checker.exe -b master -l github.com/ydcool/QrModule -au ydcool -ak c75c7ff10223322029b467945bc3bb07faaac0d0 -o errors.txt
+2019/11/01 00:32:14 🚀 start scan ...
+2019/11/01 00:32:14 🔗 136 files total in this repo...
+2019/11/01 00:32:15 🕑 start scan README.md...
+2019/11/01 00:32:16 ❌  https://api.bintray.com/packages/ydcool/maven/QrModule/images/download.svg
+2019/11/01 00:32:17 ❌  https://travis-ci.org/Ydcool/QrModule.svg?branch=master
+2019/11/01 00:32:36 ❌  http://developer.android.com/reference/android/graphics/PorterDuff.Mode.html
+2019/11/01 00:32:36 🏁 All done! broken links in total: 3
+2019/11/01 00:32:36 💌 All broken links saved toerrors.txt
+```
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /**
-./dead-link-checker.exe -b master -l github.com/ydcool/QrModule -au ydcool -ak c75c7ff10223322029b467945bc3bb07faaac0d0 -p http://127.0.0.1:3398
+./dead-link-checker.exe -v -b master -l github.com/ydcool/QrModule -au ydcool -ak c75c7ff10223322029b467945bc3bb07faaac0d0 -p http://127.0.0.1:3398 -o brokenlinks.txt
 */
 
 package main
@@ -20,6 +20,8 @@ import (
 	"sync"
 )
 
+const HttpReg = `((http|ftp|https)://)(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\&%_\./-~-]*)?`
+
 var (
 	help        bool
 	repo        string
@@ -27,6 +29,8 @@ var (
 	proxy       string
 	accessUser  string
 	accessToken string
+	output      string
+	verbose     bool
 )
 
 func init() {
@@ -40,6 +44,8 @@ Usage:
 	flag.StringVar(&proxy, "p", "", `http/https proxy`)
 	flag.StringVar(&accessUser, "au", "", `access username for higher requests rate`)
 	flag.StringVar(&accessToken, "ak", "", `access token for higher requests rate`)
+	flag.StringVar(&output, "o", "", `write result to file`)
+	flag.BoolVar(&verbose, "v", false, "print all checked links, default false to show broken only")
 	flag.BoolVar(&help, "h", false, "help")
 	flag.Usage = func() {
 		fmt.Print(helpStr)
@@ -49,14 +55,16 @@ Usage:
 
 func main() {
 	var (
-		err error
-		wg  sync.WaitGroup
+		err       error
+		wg        sync.WaitGroup
+		errorsCol = make([]string, 0)
 	)
 	flag.Parse()
 	if help || len(os.Args) == 1 {
 		flag.Usage()
 		return
 	}
+
 	linkReg, err := regexp.Compile(`github.com/.+/.+$`)
 	if err != nil {
 		log.Fatal(err)
@@ -67,6 +75,7 @@ func main() {
 	}
 	rs := strings.Split(repoLinks[0], "/")
 	log.Println("🚀 start scan ...")
+
 	rData, err := DoRequest(fmt.Sprintf("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1", rs[1], rs[2], branch))
 	if err != nil {
 		log.Fatal(err)
@@ -81,10 +90,12 @@ func main() {
 		return
 	}
 	log.Printf("🔗 %d files total in this repo...\n", len(resultData.Tree))
-	reg, err := regexp.Compile(`((http|ftp|https)://)(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\&%_\./-~-]*)?`)
+
+	reg, err := regexp.Compile(HttpReg)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, t := range resultData.Tree {
 		if t.Type == "blob" && strings.HasSuffix(t.Path, ".md") {
 			rData, err := DoRequest(t.Url)
@@ -110,18 +121,27 @@ func main() {
 					wg.Add(1)
 					go func(link string) {
 						defer wg.Done()
-						s := "✔"
 						if !DoPing(link) {
-							s = "❌"
+							log.Printf("❌  %s\n", link)
+							errorsCol = append(errorsCol, link)
+						} else if verbose {
+							log.Printf("✔ %s\n", link)
 						}
-						log.Printf("[%-2s] %s\n", s, link)
 					}(l)
 				}
 			}
 		}
 	}
 	wg.Wait()
-	log.Println("🏁 All done!")
+	log.Println("🏁 All done! broken links in total:", len(errorsCol))
+
+	if output != "" {
+		err = ioutil.WriteFile(output, []byte(strings.Join(errorsCol, "\n")), os.FileMode(os.O_CREATE|os.O_WRONLY))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print("💌 All broken links saved to ", output)
+	}
 }
 
 func getHttpClient(p string) *http.Client {
@@ -172,10 +192,10 @@ func DoRequest(link string) (string, error) {
 
 func DoPing(link string) bool {
 	req, err := http.NewRequest(http.MethodHead, link, nil)
-	addAccessHeader(accessUser, accessToken, req)
 	if err != nil {
 		return false
 	}
+	addAccessHeader(accessUser, accessToken, req)
 	var resp *http.Response
 	resp, err = getHttpClient(proxy).Do(req)
 	if err != nil {
